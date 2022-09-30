cf0b5ad2412bc92657a913d5a6166a3f30c38ea1
lanyaragao <anderlany.santos@me.com.br>
2022-09-30 18:57:09 -0300

    Screen renders two lists that pull data from two different components

---
+++ vuex-playg/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
+  </div>
+</template>
+
+<script>
+
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
+
+export default {
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  name: 'app',
+  data () {
+    return {
+      products: [
+        {name: 'Banana', price: 20},
+        {name: 'Apple', price: 40},
+        {name: 'Lemon', price: 60},
+        {name: 'Pineapple', price: 80},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
